0ba66eb985501c94c17b3144e0aa901a47b00a09
Adarsh <itsadarsh0702@gmail.com>
2024-07-16 00:16:32 +0530

    completed the site work, will do some changes if required

--- script.js
+++ script.js
@@ -36,7 +36,24 @@ function changeImage(event, imgSrc) {
 	projectImage.src = imgSrc;
 }
 
-// // Auto slide (Optional)
-// setInterval(() => {
-// 	showSlides((slideIndex += 1));
-// }, 3000);
+// Get the button and the popup elements
+var btn = document.getElementById("contactBtn");
+var popup = document.getElementById("contactPopup");
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the popup
+btn.onclick = function () {
+	popup.style.display = "block";
+};
+
+// When the user clicks on <span> (x), close the popup
+span.onclick = function () {
+	popup.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the popup, close it
+window.onclick = function (event) {
+	if (event.target == popup) {
+		popup.style.display = "none";
+	}
+};
